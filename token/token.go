f83410f4e600c7b520944b19a03d3fed80845322
Rory O'Hare <roryeoinohare@gmail.com>
2025-01-24 16:53:12 +0000

    added TokenTypes as constants

--- token/token.go
+++ token/token.go
@@ -8,3 +8,29 @@ type Token struct {
     Type    TokenType
     Literal string
 }
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    // Identifiers + literals
+    IDENT = "IDENT" // variable names
+    INT = "INT" // 12345
+    
+    // Operators
+    ASSIGN = "="
+    PLUS = "+"
+
+    // Delimiters
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    // Keywords
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+)
